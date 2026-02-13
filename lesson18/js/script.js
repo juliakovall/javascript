2b90e6784321236bf202944237687dd5f0762df6
juliakovall <jacehamster8@gmail.com>
2026-02-13 18:24:48 +0200

    I created variables, formatted the number to 2 decimal places, and used BigInt.

---
+++ lesson18/js/script.js
@@ -0,0 +1,22 @@
+let myNum = 10;
+let myStr = "some string";
+let myBool = true;
+let myArr = [1, 2, 3, 4, 5];
+let myObj = {
+  first: "First Name",
+  last: "Last Name",
+};
+
+console.log(myNum);
+console.log(myStr);
+console.log(myBool);
+console.log(myArr);
+console.log(myObj);
+
+let decimal2 = myNum.toFixed(2);
+console.log(decimal2);
+
+let myBigInt = 123n;
+myBigInt = myBigInt + 1n;
+
+console.log(myBigInt);
