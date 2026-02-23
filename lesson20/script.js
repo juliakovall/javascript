5d7e4a6fabab69034e5630adba3d7e8eda5d12c7
juliakovall <jacehamster8@gmail.com>
2026-02-23 17:26:30 +0200

    Worked with functions.

--- lesson20/script.js
+++ lesson20/script.js
@@ -7,3 +7,83 @@ const userObj = {
     return this.firstName + " " + this.lastName;
   },
 };
+function defUpperStr(str) {
+  return (str || "default text").toUpperCase();
+}
+function evenFn(n) {
+  const result = [];
+
+  for (let i = 0; i <= n; i++) {
+    if (i !== 0 && i % 2 === 0) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+function weekFn(n) {
+  if (typeof n !== "number" || !Number.isInteger(n)) return null;
+
+  switch (n) {
+    case 1:
+      return "Понеділок";
+    case 2:
+      return "Вівторок";
+    case 3:
+      return "Середа";
+    case 4:
+      return "Четвер";
+    case 5:
+      return "П’ятниця";
+    case 6:
+      return "Субота";
+    case 7:
+      return "Неділя";
+    default:
+      return null;
+  }
+}
+function ageClassification(n) {
+  return n < 0 || n > 122
+    ? null
+    : n <= 24
+      ? "Дитинство"
+      : n <= 44
+        ? "Молодість"
+        : n <= 65
+          ? "Зрілість"
+          : n <= 75
+            ? "Старість"
+            : n <= 90
+              ? "Довголіття"
+              : "Рекорд";
+}
+function oddFn(n) {
+  const result = [];
+  let i = 0;
+
+  while (i <= n) {
+    if (i !== 0 && i % 2 !== 0) {
+      result.push(i);
+    }
+    i++;
+  }
+
+  return result;
+}
+function mainFunc(a, b, cb) {
+  if (typeof cb !== "function") return false;
+  return cb(a, b);
+}
+
+function cbRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function cbPow(num, pow) {
+  return Math.pow(num, pow);
+}
+
+function cbAdd(a, b) {
+  return a + b;
+}
