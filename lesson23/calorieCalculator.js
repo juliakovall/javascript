be5b0470c67b31190bb32dfb44087ed4907e1465
juliakovall <jacehamster8@gmail.com>
2026-03-02 17:28:53 +0200

    Completed the project

--- lesson23/calorieCalculator.js
+++ lesson23/calorieCalculator.js
@@ -1,1 +1,32 @@
 console.log("#6. JavaScript homework example file");
+class CalorieCalculator {
+  constructor() {
+    this.products = new Map();
+  }
+
+  addProduct(name, calories) {
+    this.products.set(name, calories);
+  }
+
+  getProductCalories(productName) {
+    if (this.products.has(productName)) {
+      return this.products.get(productName);
+    }
+    return "Product not found";
+  }
+
+  removeProduct(productName) {
+    this.products.delete(productName);
+  }
+}
+export { CalorieCalculator };
+
+const calorieCalculator = new CalorieCalculator();
+calorieCalculator.addProduct("Apple", 52);
+calorieCalculator.addProduct("Banana", 89);
+
+console.log(calorieCalculator.getProductCalories("Apple"));
+console.log(calorieCalculator.getProductCalories("Banana"));
+
+calorieCalculator.removeProduct("Apple");
+console.log(calorieCalculator.getProductCalories("Apple"));
