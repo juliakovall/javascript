1bb0e007faeac9ab5b592d315e8f992d30ca7803
juliakovall <jacehamster8@gmail.com>
2026-03-16 14:27:17 +0200

    DateCalculator

---
+++ lesson24/calculator.js
@@ -0,0 +1,25 @@
+function DateCalculator(initialDate) {
+  this.currentDate = new Date(initialDate);
+
+  this.addDays = function (days) {
+    this.currentDate.setDate(this.currentDate.getDate() + days);
+  };
+
+  this.subtractDays = function (days) {
+    this.currentDate.setDate(this.currentDate.getDate() - days);
+  };
+
+  this.getResult = function () {
+    const year = this.currentDate.getFullYear();
+    const month = String(this.currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(this.currentDate.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  };
+}
+// const dateCalculator = new DateCalculator('2023-01-01')
+// dateCalculator.addDays(5)
+// console.log(dateCalculator.getResult())
+//
+// dateCalculator.subtractDays(3)
+// console.log(dateCalculator.getResult())
