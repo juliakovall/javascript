23f1164a171ee29c274b89e4eb22ddc7e4e4e191
juliakovall <jacehamster8@gmail.com>
2026-03-18 15:32:53 +0200

    Prepared the project for development.

---
+++ lesson27/js/main.js
@@ -0,0 +1,17 @@
+import toastLib from "/js/toast.js";
+import { initSlider } from "/js/slider.js";
+
+const toasts = toastLib("top-left");
+
+toasts.renderToast("Hello, World!", "success");
+toasts.renderToast("Hello, World 2!", "warning");
+
+const slider = initSlider({
+  selector: "#slider",
+  autoplay: true,
+  autoplaySpeed: 1000,
+  withDots: false,
+  withArrows: false,
+});
+
+export const getSum = (a, b) => a + b;
