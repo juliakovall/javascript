523ff9b2e83080a3cd554459b4868789483d04da
juliakovall <jacehamster8@gmail.com>
2026-04-23 16:21:28 +0300

    Completed

--- lesson33/src/main.js
+++ lesson33/src/main.js
@@ -1,68 +1,19 @@
-const weatherForm = document.querySelector("#weatherForm");
-const city = weatherForm.querySelector('input[name="city"]');
-const btn = weatherForm.querySelector('button[name="submit-button"]');
-const weatherContainer = document.querySelector("#weather-data");
+import "./scss/style.scss";
+import weatherIcon from "./img/premium_vector.jpg";
 
-const API_KEY = "8967388ef15aab5dc6624ae602ea61c9";
+console.log("Webpack project is working");
 
-weatherForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
+const icon = document.getElementById("weather-icon");
 
-  const cityValue = city.value.trim();
+if (icon) {
+  icon.src = weatherIcon;
+}
 
-  if (!cityValue) {
-    alert("Enter city name");
-    return;
-  }
+const weatherForm = document.getElementById("weatherForm");
 
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${API_KEY}&units=metric&lang=ua`,
-    );
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      throw new Error(data.message || "City not found");
-    }
-
-    renderWeather(data);
-  } catch (error) {
-    console.error(error);
-    alert(error.message);
-  }
-});
-
-function renderWeather(data) {
-  weatherContainer.classList.remove("hidden");
-
-  console.log(data);
-
-  weatherContainer.querySelector("#weather-city").innerText = data.name;
-
-  weatherContainer.querySelector("#weather-description").innerText =
-    data.weather[0].description;
-
-  weatherContainer.querySelector("#weather-temperature").innerText =
-    `Температура: ${Math.round(data.main.temp)}°C`;
-
-  weatherContainer.querySelector("#weather-feels-like").innerText =
-    `Відчувається як: ${Math.round(data.main.feels_like)}°C`;
-
-  weatherContainer.querySelector("#weather-humidity").innerText =
-    `Вологість: ${data.main.humidity}%`;
-
-  weatherContainer.querySelector("#weather-wind").innerText =
-    `Вітер: ${data.wind.speed} м/с`;
-
-  weatherContainer
-    .querySelector("#weather-icon")
-    .setAttribute(
-      "src",
-      `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-    );
-
-  weatherContainer
-    .querySelector("#weather-icon")
-    .setAttribute("alt", data.weather[0].description);
+if (weatherForm) {
+  weatherForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert("Form works");
+  });
 }
